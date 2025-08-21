2b927479fdde02800cf6d303f683feaff817bd3d
Tejasva Kant Saini <tsaini0408@gmail.com>
2025-08-21 23:33:03 +0530

    some changes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,6 +6,8 @@ export default function Page() {
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
         <Button size="sm">Button</Button>
+        
+
       </div>
     </div>
   )
